430cc4f1c1e79d7f14efe1645b594ec12396ded9
Josh Lemer <Josh.Lemur@marriott-sp.com>
2020-01-14 10:25:21 -0600

    Add unit tests for TopLevelParser

---
+++ language/src/test/scala/com/yoppworks/ossum/riddl/language/TopLevelParserTest.scala
@@ -0,0 +1,62 @@
+package com.yoppworks.ossum.riddl.language
+
+import java.io.File
+import java.io.FileReader
+
+import com.yoppworks.ossum.riddl.language.AST.Domain
+import com.yoppworks.ossum.riddl.language.AST.Identifier
+import com.yoppworks.ossum.riddl.language.AST.Location
+import com.yoppworks.ossum.riddl.language.AST.RootContainer
+import org.scalatest.MustMatchers
+import org.scalatest.WordSpec
+
+import scala.io.Source
+
+class TopLevelParserTest extends WordSpec with MustMatchers {
+
+  val simpleDomainFile = new File(
+    "language/src/test/input/domains/simpleDomain.riddl"
+  )
+
+  val simpleDomain = RootContainer(
+    List(
+      Domain(
+        (1, 1),
+        Identifier((1, 8), "foo"),
+        List(),
+        List(),
+        List(),
+        List(),
+        List(),
+        None
+      )
+    )
+  )
+
+  "parse" should {
+    "parse RiddlParserInput" in {
+      TopLevelParser.parse(RiddlParserInput(simpleDomainFile)) mustBe Right(
+        simpleDomain
+      )
+    }
+    "parse File" in {
+      TopLevelParser.parse(simpleDomainFile) mustBe Right(simpleDomain)
+    }
+    "parse Path" in {
+      TopLevelParser.parse(simpleDomainFile.toPath) mustBe Right(simpleDomain)
+    }
+    "parse String" in {
+      val source = Source.fromFile(simpleDomainFile)
+      try {
+        val stringContents = source.mkString
+        TopLevelParser.parse(stringContents) mustBe Right(simpleDomain)
+      } finally {
+        source.close()
+      }
+    }
+    "parse empty String" in {
+      TopLevelParser.parse("") mustBe Right(RootContainer.empty)
+    }
+  }
+
+}
