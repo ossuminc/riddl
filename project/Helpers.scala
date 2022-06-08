1a78cd1cc232c935d1ab4726d72135cf28f79b0d
Reid Spencer <reid@reactific.com>
2022-06-08 09:52:48 -0400

    Enable publishing of snapshots to Sonatype Nexus

--- project/Helpers.scala
+++ project/Helpers.scala
@@ -1,7 +1,13 @@
 import sbt.Keys._
+
 import sbt._
 import sbt.io.Path.allSubpaths
-import scoverage.ScoverageKeys.{coverageEnabled, coverageFailOnMinimum, coverageMinimumBranchTotal, coverageMinimumStmtTotal}
+import scoverage.ScoverageKeys.coverageEnabled
+import scoverage.ScoverageKeys.coverageFailOnMinimum
+import scoverage.ScoverageKeys.coverageMinimumBranchTotal
+import scoverage.ScoverageKeys.coverageMinimumStmtTotal
+import sbtdynver.DynVerPlugin.autoImport.dynverSonatypeSnapshots
+import sbtdynver.DynVerPlugin.autoImport.dynverSeparator
 
 /** V - Dependency Versions object */
 object V {
@@ -43,7 +49,11 @@ object C {
     )
   }
 
-  private def makeThemeResource(name: String, from: File, targetDir: File): Seq[File] = {
+  private def makeThemeResource(
+    name: String,
+    from: File,
+    targetDir: File
+  ): Seq[File] = {
     val zip = name + ".zip"
     val distTarget = targetDir / zip
     IO.copyDirectory(from, targetDir)
@@ -52,8 +62,7 @@ object C {
     Seq(distTarget)
   }
 
-
-  def zipResource(srcDir: String) (p :Project): Project = {
+  def zipResource(srcDir: String)(p: Project): Project = {
     p.settings(
       Compile / resourceGenerators += Def.task {
         val projectName = name.value
@@ -61,55 +70,48 @@ object C {
         val targetDir = target.value / "dist"
         makeThemeResource(projectName, from, targetDir)
       }.taskValue,
-      Compile / packageDoc / publishArtifact := false,
+      Compile / packageDoc / publishArtifact := false
     )
   }
 
   def mavenPublish(p: Project): Project = {
     p.settings(
+      ThisBuild / dynverSonatypeSnapshots := true,
+      ThisBuild / dynverSeparator := "-",
       organization := "com.reactific",
       organizationName := "Reactific Software LLC",
       organizationHomepage := Some(url("https://riddl.tech")),
-
       scmInfo := Some(ScmInfo(
-          url("https://github.com/reactific/riddl"),
-          "scm:git@github.reactific/riddl.git"
-        )
-      ),
-
-      developers := List(
-        Developer(
-          id    = "reid-spencer",
-          name  = "Reid Spencer",
-          email = "reid@reactific.com",
-          url   = url("https://riddl.tech")
-        )
-      ),
-
+        url("https://github.com/reactific/riddl"),
+        "scm:git@github.reactific/riddl.git"
+      )),
+      developers := List(Developer(
+        id = "reid-spencer",
+        name = "Reid Spencer",
+        email = "reid@reactific.com",
+        url = url("https://riddl.tech")
+      )),
       description :=
         """RIDDL is a language and toolset for specifying a system design using ideas from
           |DDD, reactive architecture, distributed systems patterns, and other software
-          |architecture practices."""
-          .stripMargin,
-      licenses := List("Apache License, Version 2.0" ->
-        new URL("https://www.apache.org/licenses/LICENSE-2.0")),
+          |architecture practices.""".stripMargin,
+      licenses := List(
+        "Apache License, Version 2.0" ->
+          new URL("https://www.apache.org/licenses/LICENSE-2.0")
+      ),
       homepage := Some(url("https://riddl.tech")),
 
       // Remove all additional repository other than Maven Central from POM
       pomIncludeRepository := { _ => false },
-
       publishTo := {
-        val nexus = "https://s01.oss.sonatype.org/"
+        val nexus = "https://oss.sonatype.org/"
         if (isSnapshot.value) {
           Some("snapshots" at nexus + "content/repositories/snapshots")
-        }
-        else {
+        } else {
           Some("releases" at nexus + "service/local/staging/deploy/maven2")
         }
       },
-
       publishMavenStyle := true,
-
       versionScheme := Some("early-semver")
     )
   }
