feb0772e7e5b4c40b425d7beee3d7dfe8bd715ae
Reid Spencer <reid@reactific.com>
2022-06-24 08:09:02 -0500

    #20 Factor out a TextFileWriter, add test case

---
+++ kalix/src/test/scala/com/reactific/riddl/translator/kalix/GrpcWriterTest.scala
@@ -0,0 +1,9 @@
+package com.reactific.riddl.translator.kalix
+
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class GrpcWriterTest extends AnyWordSpec with Matchers {
+
+  "GrpcWriter" must { "have a test" in { pending } }
+}
