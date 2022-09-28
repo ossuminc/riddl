258ec177ac27f4109e98a5cba5e7ff768abd9942
alexweinstein <alex.weinstein@yoppworks.com>
2022-09-28 10:23:36 -0400

    Namespace test pt 2

--- testkit/src/test/scala/com/reactific/riddl/language/NamespaceTest.scala
+++ testkit/src/test/scala/com/reactific/riddl/language/NamespaceTest.scala
@@ -2,27 +2,39 @@ package com.reactific.riddl.language
 
 import com.reactific.riddl.commands.CommandPlugin
 import com.reactific.riddl.testkit.RunCommandSpecBase
-import org.scalatest.Assertion
 
 /** Unit Tests For Includes */
 class NamespaceTest extends RunCommandSpecBase {
-  "FooBar" should {
-    "be validated" in {
+  "FooBarSameDomain" should {
+    "error w/ highest severity level 5" in {
       val args = Array[String](
-        "--quiet",
-        "--suppress-missing-warnings",
         "--suppress-style-warnings",
         "validate",
-        "examples/src/riddl/FooBar"
+        "examples/src/riddl/FooBarSameDomain/FooBar.riddl"
       )
-      runCommand(args)
+      CommandPlugin.runMain(args) mustEqual 5
     }
   }
 
-  def runCommand(
-    args: Array[String] = Array.empty[String]
-  ): Assertion = {
-    val rc = CommandPlugin.runMain(args)
-    rc mustBe 0
+  "FooBarTwoDomains" should {
+    "error w/ highest severity level 5" in {
+      val args = Array[String](
+        "--suppress-style-warnings",
+        "validate",
+        "examples/src/riddl/FooBarTwoDomains/FooBar.riddl"
+      )
+      CommandPlugin.runMain(args) mustEqual 5
+    }
+  }
+
+  "FooBarSuccess" should {
+    "succeed in compilation" in {
+      val args = Array[String](
+        "--suppress-style-warnings",
+        "validate",
+        "examples/src/riddl/FooBarSuccess/FooBar.riddl"
+      )
+      CommandPlugin.runMain(args) mustEqual 0
+    }
   }
 }
