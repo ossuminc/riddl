c94f732e31f96c23694712185f5048cefc6b6e1d
Scala Steward <scala_steward@virtuslab.com>
2023-06-09 01:50:48 +0000

    Update commons-io to 2.13.0

--- project/Helpers.scala
+++ project/Helpers.scala
@@ -13,7 +13,7 @@ import sbtdynver.DynVerPlugin.autoImport.dynverVTagPrefix
 
 /** V - Dependency Versions object */
 object V {
-  val commons_io = "2.12.0"
+  val commons_io = "2.13.0"
   val compress = "1.23.0"
   val config = "1.4.2"
   val fastparse = "3.0.1"
