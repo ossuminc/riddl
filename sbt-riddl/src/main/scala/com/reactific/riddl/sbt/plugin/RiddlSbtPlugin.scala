b506868fe3446d1ce230581e8fa797fae6cdb1d1
reidspencer <reid.spencer@yoppworks.com>
2023-09-28 10:24:56 +0200

    Don't rely on a Tuple for proper hash function
    Signed-off-by: reidspencer <reid.spencer@yoppworks.com>

--- sbt-riddl/src/main/scala/com/reactific/riddl/sbt/plugin/RiddlSbtPlugin.scala
+++ sbt-riddl/src/main/scala/com/reactific/riddl/sbt/plugin/RiddlSbtPlugin.scala
@@ -78,11 +78,32 @@ object RiddlSbtPlugin extends AutoPlugin {
     Compile / compile := Def.taskDyn {
       val c = (Compile / compile).value
       Def.task {
-        val _ = (Compile / compileTask).value
+        val command = s"riddlc from ${riddlcConf.value} validate"
+        val _ = runCommandAndRemaining(command)
         c
       }
     }.value
   )
+
+  /**
+   * Convert the given command string to a release step action, preserving and invoking remaining commands
+   * Note: This was copied from https://github.com/sbt/sbt-release/blob/663cfd426361484228a21a1244b2e6b0f7656bdf/src/main/scala/ReleasePlugin.scala#L99-L115
+   */
+  def runCommandAndRemaining(command: String): State => State = { st: State =>
+    import sbt.complete.Parser
+    @annotation.tailrec
+    def runCommand(command: String, state: State): State = {
+      val nextState = Parser.parse(command, state.combinedParser) match {
+        case Right(cmd) => cmd()
+        case Left(msg) => throw sys.error(s"Invalid programmatic input:\n$msg")
+      }
+      nextState.remainingCommands.toList match {
+        case Nil => nextState
+        case head :: tail => runCommand(head.commandLine, nextState.copy(remainingCommands = tail))
+      }
+    }
+    runCommand(command, st.copy(remainingCommands = Nil)).copy(remainingCommands = st.remainingCommands)
+  }
 
   private def versionTriple(version: String): (Int, Int, Int) = {
     val trimmed = version.indexOf('-') match {
