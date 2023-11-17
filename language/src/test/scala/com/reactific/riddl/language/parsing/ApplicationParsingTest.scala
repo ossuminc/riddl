2a932f3bccfc48f9b3cccb639d2d3a4b14fcdcab
reidspencer <reid.spencer@yoppworks.com>
2023-11-17 14:49:04 -0500

    Clean up test case syntax
    Signed-off-by: reidspencer <reid.spencer@yoppworks.com>

--- language/src/test/scala/com/reactific/riddl/language/parsing/ApplicationParsingTest.scala
+++ language/src/test/scala/com/reactific/riddl/language/parsing/ApplicationParsingTest.scala
@@ -5,25 +5,26 @@ import com.reactific.riddl.language.Messages.*
 import com.reactific.riddl.language.ParsingTest
 import org.scalatest.matchers.must.Matchers
 
-class ApplicationParsingTest extends ParsingTest with Matchers{
+class ApplicationParsingTest extends ParsingTest with Matchers {
 
   "Application Components" must {
     "empty definitions should fail" in {
       val input =
         """
           |domain foo {
-          |application fooer {
+          |application foo2 {
           |  group {}
           |}
           |}""".stripMargin
       parseDefinition[Domain](input) match {
-        case Left(msgs: Messages) =>
-          val errors = msgs.justErrors
-          errors.size mustBe(1)
+        case Left(messages: Messages) =>
+          val errors = messages.justErrors
+          errors.size mustBe 1
           val msg = errors.head
-          msg.message contains("Expected one of")
-          msg.message contains("???")
-          msg.loc.line mustBe(4)
+          msg.message contains "Expected one of"
+          msg.message contains "???"
+          msg.loc.line mustBe 4
+
         case Right((dom: Domain, _)) =>
           fail("should have required ??? syntax")
       }
