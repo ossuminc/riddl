8c54636ef7919806e6274fec57eff5b800f9b42d
reidspencer <reid.spencer@yoppworks.com>
2024-01-12 13:01:42 -0500

    Fix merge compilation error

--- diagrams/src/test/scala/com/ossuminc/riddl/diagrams/mermaid/UseCaseDiagramTest.scala
+++ diagrams/src/test/scala/com/ossuminc/riddl/diagrams/mermaid/UseCaseDiagramTest.scala
@@ -2,7 +2,7 @@ package com.ossuminc.riddl.diagrams.mermaid
 
 import com.ossuminc.riddl.diagrams.{DiagramsPass, DiagramsPassOutput}
 import com.ossuminc.riddl.diagrams.UseCaseDiagramData
-import com.ossuminc.riddl.language.AST.Definition
+import com.ossuminc.riddl.language.AST.NamedValue
 import com.ossuminc.riddl.language.parsing.RiddlParserInput
 import com.ossuminc.riddl.testkit.RunPassTestBase
 import com.ossuminc.riddl.passes.PassesResult
@@ -14,10 +14,10 @@ class UseCaseDiagramTest extends RunPassTestBase {
 
   case class TestUCDSupport(passesResult: PassesResult) extends UseCaseDiagramSupport {
     val symTab = passesResult.symbols
-    def makeDocLink(definition: Definition): String = {
-      val parents = symTab.parentsOf(definition)
+    def makeDocLink(namedValue: NamedValue): String = {
+      val parents = symTab.parentsOf(namedValue)
       val uri = new URI(
-        "http", "example.com", parents.map(_.id.value).mkString("//", "/", "/") + s"${definition.id.value}", null, null
+        "http", "example.com", parents.map(_.id.value).mkString("//", "/", "/") + s"${namedValue.id.value}", null, null
       )
       uri.toASCIIString
     }
