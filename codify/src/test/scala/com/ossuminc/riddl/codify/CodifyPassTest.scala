29e9193b41362ad434426cb94e952569a67b9442
reidspencer <reid.spencer@yoppworks.com>
2024-04-03 12:55:43 -0400

    Start work on testing the codify pass

---
+++ codify/src/test/scala/com/ossuminc/riddl/codify/CodifyPassTest.scala
@@ -0,0 +1,31 @@
+package com.ossuminc.riddl.codify
+
+import com.ossuminc.riddl.language.parsing.RiddlParserInput
+import com.ossuminc.riddl.language.Messages.*
+import com.ossuminc.riddl.testkit.ValidatingTest 
+
+import java.nio.file.Path 
+
+class CodifyPassTest extends ValidatingTest {
+
+  def input(path: String): RiddlParserInput = {
+    RiddlParserInput(Path.of("codify/src/test/input/", path))
+  }
+
+  "CodifyPass" must {
+
+    "run" in {
+      val rpi = input("VendingMachine.riddl")
+      parseTopLevelDomains(rpi) match {
+        case Left(messages) =>
+          val errors = messages.justErrors
+          if errors.nonEmpty then fail(errors.format)
+          info(messages.justWarnings.format)
+          succeed
+        case Right(root) =>
+          succeed
+      }
+    }
+  }
+
+}
