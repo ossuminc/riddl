aad8a702e162b222a538d42d70aeea28dca994a2
reidspencer <reid.spencer@yoppworks.com>
2024-05-01 21:32:44 -0400

    Fix ScalaDoc warnings

--- language/src/main/scala/com/ossuminc/riddl/language/Finder.scala
+++ language/src/main/scala/com/ossuminc/riddl/language/Finder.scala
@@ -8,18 +8,18 @@ package com.ossuminc.riddl.language
 
 import com.ossuminc.riddl.language.AST.*
 
-/** The context for finding things within a given [Ccontainer]] or [[RiddlValue]] as found in the AST model. This
+/** The context for finding things within a given [Ccontainer]] or [[AST.RiddlValue]] as found in the AST model. This
   * provides the ability to find values in the model by traversing it and looking for the matching condition.
   * @param root
   *   The container of RiddlValues to traverse for the sought condition
   */
 case class Finder(root: Container[RiddlValue]) {
 
-  /** Search the `root` for a RiddlValue that matches the boolean expression
+  /** Search the `root` for a [[AST.RiddlValue]] that matches the boolean expression
     * @param select
     *   The boolean expression to search for
     * @return
-    *   A [[scala.Seq]] of the matching [[RiddlValue]]
+    *   A [[scala.Seq]] of the matching [[AST.RiddlValue]]
     */
   def find(select: RiddlValue => Boolean): Seq[RiddlValue] = {
     Folding.foldEachDefinition(root, root, Seq.empty) { case (_, value, state) =>
@@ -27,16 +27,16 @@ case class Finder(root: Container[RiddlValue]) {
     }
   }
 
-  /** THe return value for the [[findWithParents()]] function */
+  /** THe return value for the [[Finder.findWithParents()]] function */
   type DefWithParents = Seq[(RiddlValue, Seq[RiddlValue])]
 
-  /** Find a matching set of [[RiddlValue]] but return them with their parents
+  /** Find a matching set of [[AST.RiddlValue]] but return them with their parents
     *
     * @param select
-    *   The boolean expression derived from a candidate [[RiddlValue]] that selects it to the result set
+    *   The boolean expression derived from a candidate [[AST.RiddlValue]] that selects it to the result set
     * @return
-    *   A [[Finder#DefWithParents]] that returns a [[scala.Seq]] of two-tuples with the [[RiddlValue]] a a [[scala.Seq]]
-    *   of the parents of that value.
+    *   A [[Finder#DefWithParents]] that returns a [[scala.Seq]] of two-tuples with 
+    *   the [[AST.RiddlValue]] a a [[scala.Seq]] of the parents of that value.
     */
   def findWithParents(
     select: RiddlValue => Boolean
@@ -49,7 +49,7 @@ case class Finder(root: Container[RiddlValue]) {
   /** Find definitions that are empty
     *
     * @return
-    *   A [[scala.Seq]] of [[RiddlValue]], along with their parents that are empty
+    *   A [[scala.Seq]] of [[AST.RiddlValue]], along with their parents that are empty
     */
   def findEmpty: DefWithParents = findWithParents(_.isEmpty)
 }
