995538bc8556345520dacb859b09b0c6c9558c21
Reid Spencer <reid@reactific.com>
2024-05-04 12:17:25 -0400

    Start a check of the StatsPass

--- analyses/src/test/scala/com/ossuminc/riddl/analyses/StatsPassTest.scala
+++ analyses/src/test/scala/com/ossuminc/riddl/analyses/StatsPassTest.scala
@@ -1,12 +1,22 @@
 package com.ossuminc.riddl.analyses
 
 import com.ossuminc.riddl.passes.validate.ValidatingTest
+import org.scalatest.*
 
 class StatsPassTest extends ValidatingTest {
 
-  "StatsPass" must {
+  "StatsPass" should  {
     "generate statistics" in {
-      // TODO: write me
+/*      def parseAndValidate(
+        input: String,
+        origin: String,
+        options: CommonOptions = CommonOptions(),
+        shouldFailOnErrors: Boolean = true
+      )(
+        validation: (Root, Messages) => Assertion
+      val pass = StatsPass(input, outputs)
+ */
     }
+
   }
 }
