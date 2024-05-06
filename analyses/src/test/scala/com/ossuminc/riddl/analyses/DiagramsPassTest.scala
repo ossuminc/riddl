d87d156756ffc5839c293f61e41d069639485684
Reid Spencer <reid@reactific.com>
2024-05-06 17:31:28 -0400

    Update DiagramsPassTest

--- analyses/src/test/scala/com/ossuminc/riddl/analyses/DiagramsPassTest.scala
+++ analyses/src/test/scala/com/ossuminc/riddl/analyses/DiagramsPassTest.scala
@@ -1,11 +1,38 @@
 package com.ossuminc.riddl.analyses
 
+import com.ossuminc.riddl.language.AST.{Domain, Identifier, Root}
+import com.ossuminc.riddl.language.At
+import com.ossuminc.riddl.passes.{PassInfo, PassInput, PassOptions, PassesOutput}
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class DiagramsPassTest extends AnyWordSpec with Matchers {
 
+  "Diagrams Data" must {
+    "construct ContextDiagramData" in {
+      val d = Domain(At(), Identifier(At(), "domain"))
+      val contextDiagramData = ContextDiagramData(d, Seq.empty, Seq.empty)
+      contextDiagramData.aggregates mustBe empty
+      contextDiagramData.domain mustBe d
+    }
+    "construct DiagramsPassOutput" in {
+      val diagramsPassOutput = DiagramsPassOutput()
+      diagramsPassOutput.messages mustBe empty
+      diagramsPassOutput.contextDiagrams mustBe empty
+      diagramsPassOutput.dataFlowDiagrams mustBe empty
+    }
+  }
   "DiagramsPass" must {
+    "be named correctly" in {
+      DiagramsPass.name mustBe "Diagrams"
+    }
+    "creator with empty PassesOutput yields IllegalArgumentException" in {
+      val creator = DiagramsPass.creator()
+      val input = PassInput(Root())
+      val outputs = PassesOutput()
+      val pass = intercept[IllegalArgumentException] {creator(input, outputs)}
+      pass.isInstanceOf[IllegalArgumentException] mustBe true
+    }
     "do something" in {
       succeed
     }
