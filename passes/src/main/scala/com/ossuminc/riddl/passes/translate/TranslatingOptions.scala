aa16ee5ab7f8826468ca57bd2a917f47bdf0fc8f
reidspencer <reid.spencer@yoppworks.com>
2024-05-07 19:23:51 -0400

    Add coverage for TranslatingState

--- passes/src/main/scala/com/ossuminc/riddl/passes/translate/TranslatingOptions.scala
+++ passes/src/main/scala/com/ossuminc/riddl/passes/translate/TranslatingOptions.scala
@@ -3,7 +3,7 @@ package com.ossuminc.riddl.passes.translate
 import java.nio.file.Path
 
 /** A base trait for the options that a TranslatingPass must have  */
-trait TranslatingOptions {
+trait TranslatingOptions  {
   def inputFile: Option[Path]
   def outputDir: Option[Path]
   def projectName: Option[String]
