be233e28b3fb42fd58a1ce0211804fb19d211330
reidspencer <reid.spencer@yoppworks.com>
2024-05-07 19:07:06 -0400

    Remove unutilized code

--- passes/src/main/scala/com/ossuminc/riddl/passes/translate/TranslatingPass.scala
+++ passes/src/main/scala/com/ossuminc/riddl/passes/translate/TranslatingPass.scala
@@ -14,24 +14,11 @@ import com.ossuminc.riddl.utils.Logger
 
 import java.nio.file.Path
 
-
 /** Base class of all Passes that translate the AST to some other form.
-  * 
+  *
   * @param input
-  * The input to be translated
+  *   The input to be translated
   * @param outputs
-  * The prior outputs from preceding passes
+  *   The prior outputs from preceding passes
   */
-abstract class TranslatingPass(input: PassInput, outputs: PassesOutput) extends Pass(input, outputs) {
-
-  def passes: PassesCreator = Seq.empty[PassCreator]
-
-  final def parseValidateTranslate(
-    input: RiddlParserInput,
-    log: Logger,
-    commonOptions: CommonOptions,
-  ): Either[Messages, PassesResult] = {
-    val allPasses = Pass.standardPasses ++ passes
-    Riddl.parseAndValidate(input, commonOptions, shouldFailOnError=true, allPasses, log )
-  }
-}
+abstract class TranslatingPass(input: PassInput, outputs: PassesOutput) extends Pass(input, outputs)
