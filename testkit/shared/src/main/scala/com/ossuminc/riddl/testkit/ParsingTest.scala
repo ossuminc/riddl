8b15c4313ad7d866a61233415a61b41d52b193e0
reidspencer <reid.spencer@yoppworks.com>
2024-09-23 14:44:22 -0400

    Make ParsingTest and ValidatingTest available

---
+++ testkit/shared/src/main/scala/com/ossuminc/riddl/testkit/ParsingTest.scala
@@ -0,0 +1,3 @@
+package com.ossuminc.riddl.testkit
+
+class ParsingTest extends com.ossuminc.riddl.language.parsing.NoJVMParsingTest
---
+++ testkit/shared/src/main/scala/com/ossuminc/riddl/testkit/ValidatingTest.scala
@@ -0,0 +1,4 @@
+package com.ossuminc.riddl.testkit
+
+class ValidatingTest extends com.ossuminc.riddl.passes.validate.NoJVMValidatingTest
+
