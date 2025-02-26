1164ccf1a800ebf0ae3e22c668a1cd73af51d5b6
reidspencer <reid.spencer@yoppworks.com>
2025-02-26 14:47:54 -0500

    Initial Design
    * Create a PromptGeneration module
    * Implement the apply method to generate text from a node

---
+++ riddlLib/shared/src/main/scala/com/ossuminc/riddl/PromptGeneration.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019-2025 Ossum, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.ossuminc.riddl
+
+import com.ossuminc.riddl.language.AST.*
+import com.ossuminc.riddl.language.Messages.Messages
+import com.ossuminc.riddl.passes.PassesResult
+import com.ossuminc.riddl.passes.symbols.{SymbolsOutput, SymbolsPass}
+
+case class PromptGeneration(passesResult: PassesResult) {
+
+  extension (sb: StringBuilder)
+    def description(definition: Definition): StringBuilder =
+      sb
+        .append("briefly described as \"")
+        .append(definition.briefString)
+        .append("\", fully described as \"")
+        .append(definition.descriptionString)
+      sb
+  end extension
+
+  def apply(definition: Definition, prompt: String): Either[Messages, String] =
+    val parents: Parents = passesResult.symbols.parentsOf(definition)
+    val stack: ParentStack = ParentStack(parents*)
+    val result = new StringBuilder
+    parents.reverse.foldLeft(result) { (sb: StringBuilder, definition: Definition) =>
+      definition match
+        case d: Domain =>
+          sb.append(s"In the knowledge Domain named ")
+            .append(d.id.format)
+            .append(", ")
+            .description(d)
+            .append(", ")
+        case c: Context =>
+          sb.append(s"In the context of ")
+            .append(c.id.format)
+            .append(",")
+            .description(c)
+            .append(", ")
+        case e: Entity =>
+          sb.append(s"For the entity named ")
+            .append(e.id.format)
+            .append(",")
+            .description(e)
+            .append(",")
+        // TODO: Write other cases
+        case _: Definition => ()
+      end match
+      sb
+    }
+    result.append(": ").append(prompt)
+    Right(result.toString)
+}
